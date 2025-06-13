f570c50e46d136d6a1dee4fdb68d583a6fb8cadf
souleymanesy7 <souleymanesyservices@gmail.com>
2025-06-14 00:18:54 +0200

    feat: update the better-auth client handler

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,13 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL!,
+  baseURL: process.env.BETTER_AUTH_URL || 'http://localhost:3000',
 });
 
-export const { signIn, signUp, signOut, useSession } = authClient;
+export const { 
+  signUp, 
+  signIn, 
+  signOut, 
+  useSession,
+  getSession 
+} = authClient;
