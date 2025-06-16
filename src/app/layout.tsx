880291a569f8dab40d842c21d10eb214616c413c
souleymanesy7 <souleymanesyservices@gmail.com>
2025-06-16 10:15:02 +0200

    feat: add the shadcn toast notification component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
-import * as React from 'react';
-import type { Metadata } from 'next';
-import './globals.css';
+import * as React from "react";
+import type { Metadata } from "next";
+
+import { Toaster } from "@/components/ui/sonner";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Business Nexus',
-  description: 'Business Nexus app',
+  title: "Business Nexus",
+  description: "Business Nexus app",
 };
 
 export default function RootLayout({
@@ -14,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
