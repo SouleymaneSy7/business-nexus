fc7d3c44ff9ccfca6839f828484f9530e5c36d10
souleymanesy7 <souleymanesyservices@gmail.com>
2025-06-17 16:30:57 +0200

    feat: update the drizzle configs

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,16 +1,12 @@
 import postgres from "postgres";
 import { config } from "dotenv";
-import { users } from "./schema/auth-schema";
 import { drizzle } from "drizzle-orm/postgres-js";
 
 config({ path: ".env" });
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL!;
 if (!connectionString) throw new Error("DATABASE_URL environment variable is not defined");
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(connectionString, { prepare: false });
 export const db = drizzle({ client, casing: "snake_case" });
-
-const allUsers = await db.select().from(users);
-console.log(allUsers);
