1efd2dc314673de1cdf99f9c7f17db76f4556c73
souleymanesy7 <souleymanesyservices@gmail.com>
2025-06-19 18:35:32 +0200

    feat: update the custom 404 component

--- src/components/shared/Custom404.tsx
+++ src/components/shared/Custom404.tsx
@@ -1,32 +1,43 @@
-import { Button } from "@components/ui/button";
-import { ArrowLeftIcon } from "lucide-react";
+import * as React from "react";
+
 import Link from "next/link";
-import React from "react";
+import { ArrowLeftIcon } from "lucide-react";
+
+import { Button } from "@components/ui/button";
+import Container from "@components/common/Container";
+import Title from "@components/common/Title";
 
 export default function Custom404() {
   return (
     <React.Fragment>
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <Container
+        as={"main"}
+        className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8"
+      >
         <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">404</p>
-          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+          <p className="text-primary text-base font-semibold">404</p>
+          <Title
+            level="h1"
+            ariaLevel={1}
+            className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl"
+          >
             Page not found
-          </h1>
+          </Title>
 
           <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-            Sorry, we couldn’t find the page you’re looking for.
+            Sorry, we couldn't find the page you're looking for.
           </p>
 
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Button asChild>
-              <Link href="/" className="text-balance flex items-center gap-3 text-gray-900">
+              <Link href="/" className="flex items-center gap-3 text-balance text-gray-900">
                 <ArrowLeftIcon />
                 Go back home
               </Link>
             </Button>
           </div>
         </div>
-      </main>
+      </Container>
     </React.Fragment>
   );
 }
