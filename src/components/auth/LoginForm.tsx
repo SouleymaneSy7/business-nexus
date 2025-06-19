eed26a2b010f8b2fe02e960982929471bc622164
souleymanesy7 <souleymanesyservices@gmail.com>
2025-06-19 18:38:30 +0200

    feat: update the login form component

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import * as React from "react";
 import * as z from "zod";
+import * as React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -14,18 +14,28 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Loader2Icon } from "lucide-react";
 import { Alert, AlertDescription } from "@components/ui/alert";
+import { GoogleIcon, GithubIcon } from "@/icons/Icons.component";
+import { Checkbox } from "@components/ui/checkbox";
+
+import Container from "@components/common/Container";
+
+type LoginFormType = z.infer<typeof loginSchema>;
 
 const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
 });
 
-type LoginFormType = z.infer<typeof loginSchema>;
-
-const LoginForm = ({ className, ...props }: React.ComponentProps<"div">) => {
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [error, setError] = React.useState<string | null>(null);
+const LoginForm = () => {
   const router = useRouter();
+
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
+
+  const id = React.useId();
+  const emailId = `${id}-email`;
+  const passwordId = `${id}-password`;
+  const rememberId = `${id}-remember`;
 
   const {
     register,
@@ -37,83 +47,94 @@ const LoginForm = ({ className, ...props }: React.ComponentProps<"div">) => {
 
   const onSubmit = async (data: LoginFormType) => {
     setIsLoading(true);
-    setError(null);
+    setErrorMessage(null);
 
     const role = "entrepreneur";
 
     try {
       router.push(`/dashboard/${role}`);
     } catch (error) {
-      setError("An error occurred during login");
+      setErrorMessage("An error occurred during login");
     } finally {
       setIsLoading(false);
     }
   };
 
   return (
-    <div>
+    <Container as={"main"} className="w-full max-w-lg">
       <Card>
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">Welcome back</CardTitle>
-          <CardDescription>Login with your Google account</CardDescription>
+          <CardTitle className="text-primary text-2xl">Welcome back</CardTitle>
+          <CardDescription>Login with your Google or Github account</CardDescription>
         </CardHeader>
+
         <CardContent>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="grid gap-6">
-              <div className="flex flex-col gap-4">
-                <Button variant="outline" className="w-full">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                      fill="currentColor"
-                    />
-                  </svg>
+              <div className="flex w-full items-center justify-center gap-3">
+                <Button variant="outline" className="grow cursor-pointer">
+                  <GoogleIcon />
                   Login with Google
                 </Button>
+
+                <Button variant={"outline"} className="grow cursor-pointer">
+                  <GithubIcon />
+                  Login with GitHub
+                </Button>
               </div>
+
               <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                 <span className="bg-card text-muted-foreground relative z-10 px-2">
                   Or continue with
                 </span>
               </div>
+
               <div className="grid gap-6">
                 <div className="grid gap-3">
-                  <Label htmlFor="email">Email</Label>
+                  <Label htmlFor={emailId}>Email</Label>
+
                   <Input
-                    id="email"
-                    type="email"
-                    placeholder="email@example.com"
                     required
+                    type="email"
+                    id={emailId}
                     disabled={isLoading}
                     {...register("email")}
+                    placeholder="email@example.com"
                   />
-                  {errors.email && <p className="text-sm text-red-500">{errors.email.message}</p>}
+                  {errors.email && (
+                    <small className="text-sm text-red-500">{errors.email.message}</small>
+                  )}
                 </div>
 
                 <div className="grid gap-3">
-                  <Label htmlFor="password">Password</Label>
+                  <Label htmlFor={passwordId}>Password</Label>
 
                   <Input
-                    id="password"
-                    type="password"
-                    placeholder="••••••••"
                     required
+                    type="password"
+                    id={passwordId}
                     disabled={isLoading}
+                    placeholder="••••••••"
                     {...register("password")}
                   />
 
                   {errors.password && (
-                    <p className="text-sm text-red-500">{errors.password.message}</p>
+                    <small className="text-sm text-red-500">{errors.password.message}</small>
                   )}
                 </div>
 
-                {error && (
+                <div className="flex items-center gap-2">
+                  <Checkbox id={rememberId} className="cursor-pointer" />
+                  <Label htmlFor={rememberId}>Remember Me</Label>
+                </div>
+
+                {errorMessage && (
                   <Alert variant="destructive">
-                    <AlertDescription>{error}</AlertDescription>
+                    <AlertDescription>{errorMessage}</AlertDescription>
                   </Alert>
                 )}
 
-                <Button type="submit" className="w-full" disabled={isLoading}>
+                <Button type="submit" className="w-full cursor-pointer" disabled={isLoading}>
                   {isLoading && <Loader2Icon className="mr-2 h-4 w-4 animate-spin" />}
                   Login
                 </Button>
@@ -129,7 +150,7 @@ const LoginForm = ({ className, ...props }: React.ComponentProps<"div">) => {
           </form>
         </CardContent>
       </Card>
-    </div>
+    </Container>
   );
 };
 
