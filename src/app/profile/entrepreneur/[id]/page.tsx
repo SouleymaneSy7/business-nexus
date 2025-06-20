ee2b4e77d041480a2a567df740faee092b0511dc
souleymanesy7 <souleymanesyservices@gmail.com>
2025-06-21 01:57:59 +0200

    feat: add the entrepreneur profile page

--- src/app/profile/entrepreneur/[id]/page.tsx
+++ src/app/profile/entrepreneur/[id]/page.tsx
@@ -1,5 +1,26 @@
-const page = () => {
-  return <div>Entrepreneur Profile Page</div>;
+import { PagePropsType } from "@/types";
+import { mockEntrepreneurs } from "@/lib/mock-data";
+
+import DashboardLayout from "@/components/dashboard/DashboardLayout";
+import EntrepreneurProfile from "@components/profile/EntrepreneurProfile";
+
+const page = async (props: { params: PagePropsType }) => {
+  const params = await props.params;
+  const entrepreneur = mockEntrepreneurs.find((event) => event.id == params.id);
+
+  if (!entrepreneur) {
+    return (
+      <DashboardLayout>
+        <div className="grid h-svh place-items-center">Investor Not Found</div>
+      </DashboardLayout>
+    );
+  }
+
+  return (
+    <DashboardLayout>
+      <EntrepreneurProfile entrepreneur={entrepreneur} />
+    </DashboardLayout>
+  );
 };
 
 export default page;
