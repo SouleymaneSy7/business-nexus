2bec9bee48fcc2e34859cc896eb1988622657f74
souleymanesy7 <souleymanesyservices@gmail.com>
2025-06-21 01:58:43 +0200

    feat: add the investor profile page

--- src/app/profile/investor/[id]/page.tsx
+++ src/app/profile/investor/[id]/page.tsx
@@ -1,5 +1,26 @@
-const page = () => {
-  return <div>Investor Profile Page</div>;
+import { PagePropsType } from "@/types";
+import { mockInvestors } from "@/lib/mock-data";
+
+import DashboardLayout from "@/components/dashboard/DashboardLayout";
+import InvestorProfile from "@components/profile/InvestorProfile";
+
+const page = async (props: { params: PagePropsType }) => {
+  const params = await props.params;
+  const investor = mockInvestors.find((event) => event.id == params.id);
+
+  if (!investor) {
+    return (
+      <DashboardLayout>
+        <div className="grid h-svh place-items-center">Investor Not Found</div>
+      </DashboardLayout>
+    );
+  }
+
+  return (
+    <DashboardLayout>
+      <InvestorProfile investor={investor} />
+    </DashboardLayout>
+  );
 };
 
 export default page;
