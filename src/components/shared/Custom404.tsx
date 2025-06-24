9a0371bdc3f5b5002e2d6c05f6f891dbe8c33bc0
souleymanesy7 <souleymanesyservices@gmail.com>
2025-06-24 21:46:30 +0200

    feat: update the custom-404 page style

--- src/components/shared/Custom404.tsx
+++ src/components/shared/Custom404.tsx
@@ -12,25 +12,25 @@ export default function Custom404() {
     <React.Fragment>
       <Container
         as={"main"}
-        className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8"
+        className="bg-background grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8"
       >
         <div className="text-center">
           <p className="text-primary text-base font-semibold">404</p>
           <Title
             level="h1"
             ariaLevel={1}
-            className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl"
+            className="text-foreground mt-4 text-5xl font-semibold tracking-tight text-balance sm:text-7xl"
           >
             Page not found
           </Title>
 
-          <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+          <p className="text-muted-foreground mt-6 text-lg font-medium text-pretty sm:text-xl/8">
             Sorry, we couldn `&apos;`t find the page you `&apos;`re looking for.
           </p>
 
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Button asChild>
-              <Link href="/" className="flex items-center gap-3 text-balance text-gray-900">
+              <Link href="/" className="flex items-center gap-3 text-balance">
                 <ArrowLeftIcon />
                 Go back home
               </Link>
