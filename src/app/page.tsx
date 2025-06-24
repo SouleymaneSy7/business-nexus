d4baeb32e67d1f1bb1b53a29546998ef5f25ad29
souleymanesy7 <souleymanesyservices@gmail.com>
2025-06-24 19:44:45 +0200

    feat: update the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,28 @@
 import Link from "next/link";
-import Container from "@components/common/Container";
-import { Ripple } from "@components/magicui/ripple";
-import { Button } from "@components/ui/button";
+
 import Title from "@components/common/Title";
+import Container from "@components/common/Container";
+
+import { Button } from "@components/ui/button";
+import { Ripple } from "@components/magicui/ripple";
 import { ChevronRightIcon } from "lucide-react";
 
 function Home() {
   return (
-    <div className="bg-background relative flex h-svh w-full flex-col items-center justify-center overflow-hidden rounded-lg border">
+    <div className="bg-background relative flex h-svh w-full flex-col items-center justify-center overflow-hidden rounded-lg border p-5">
       <Container className="z-10 flex h-full w-full flex-col items-center justify-center">
         <Title
           level="h1"
           ariaLevel={1}
-          className="text-card-foreground mb-6 text-center text-5xl font-semibold tracking-tight md:text-6xl"
+          className="text-card-foreground mb-6 text-center text-3xl font-semibold tracking-tight md:text-6xl"
         >
           Connect{" "}
-          <span className="text-animate-colors bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 bg-clip-text text-7xl font-black whitespace-pre-wrap text-transparent drop-shadow-lg md:text-7xl">
+          <span className="text-animate-colors header-bg-gradient-1 bg-clip-text text-3xl font-black whitespace-pre-wrap text-transparent drop-shadow-lg md:text-7xl">
             <i>entrepreneurs</i>
           </span>{" "}
           <br />
           and{" "}
-          <span className="text-animate-colors bg-gradient-to-r from-indigo-500 via-green-500 to-pink-500 bg-clip-text text-7xl font-black whitespace-pre-wrap text-transparent italic drop-shadow-lg md:text-7xl">
+          <span className="text-animate-colors bg-gradient-to-r from-indigo-500 via-green-500 to-pink-500 bg-clip-text text-3xl font-black whitespace-pre-wrap text-transparent italic drop-shadow-lg md:text-7xl">
             investors.
           </span>
         </Title>
@@ -31,12 +33,18 @@ function Home() {
           shape the future together.
         </p>
 
-        <div className="mt-4 flex items-center justify-center gap-4">
-          <Button asChild className="bg-primary text-primary-foreground hover:bg-primary/90 px-6 py-4.5">
+        <div className="mt-4 flex flex-wrap items-center justify-center gap-4">
+          <Button
+            asChild
+            className="bg-primary text-primary-foreground hover:bg-primary/90 px-6 py-4.5"
+          >
             <Link href="/login">Login </Link>
           </Button>
 
-          <Button asChild className="bg-primary text-primary-foreground hover:bg-primary/90 px-6 py-4.5">
+          <Button
+            asChild
+            className="bg-primary text-primary-foreground hover:bg-primary/90 px-6 py-4.5"
+          >
             <Link href="/dashboard/entrepreneur">
               Go to Dashboard
               <ChevronRightIcon className="ml-2 h-4 w-4" />
