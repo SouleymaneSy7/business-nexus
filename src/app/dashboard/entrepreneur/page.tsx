fe688f485ee861332416a50b00a14579180606ca
souleymanesy7 <souleymanesyservices@gmail.com>
2025-06-26 00:25:07 +0200

    feat: update the dashboard components

--- src/app/dashboard/entrepreneur/page.tsx
+++ src/app/dashboard/entrepreneur/page.tsx
@@ -1,20 +1,59 @@
 "use client";
 
-import { mockEntrepreneurs } from "@/lib/mock-data";
+import { mockCollaborationRequests, mockEntrepreneurs } from "@/lib/mock-data";
 
 import List from "@components/common/List";
 import Title from "@components/common/Title";
 import DashboardLayout from "@/components/dashboard/DashboardLayout";
 import EntrepreneurCard from "@components/dashboard/EntrepreneurCard";
+import CollaborationRequestCard from "@components/dashboard/CollaborationRequestCard";
 
 const Page = () => {
   return (
     <DashboardLayout>
       <div className="mx-auto max-w-7xl p-6">
+        <div className="mb-8">
+          <Title
+            level="h1"
+            ariaLevel={1}
+            className="text-card-foreground mb-2 text-4xl font-extrabold"
+          >
+            Welcome to Your Entrepreneur Dashboard
+          </Title>
+          
+          <p className="text-muted-foreground mb-4">
+            Track collaboration requests and discover new entrepreneurs to connect and grow your
+            business.
+          </p>
+        </div>
+
+        <div className="mb-6">
+          <Title level="h2" className="text-card-foreground mb-2 text-3xl font-bold">
+            Collaboration Requests
+          </Title>
+
+          <p className="text-muted-foreground mb-4">
+            Review and manage collaboration requests from entrepreneurs:
+          </p>
+        </div>
+
+        <List
+          as={"section"}
+          className="relative-grid mb-9"
+          items={mockCollaborationRequests}
+          renderItem={(collaborationRequest) => (
+            <CollaborationRequestCard
+              key={collaborationRequest.id}
+              userRequests={collaborationRequest}
+            />
+          )}
+        />
+
         <div className="mb-6">
           <Title level="h3" ariaLevel={3} className="text-card-foreground mb-2 text-3xl font-bold">
             Discover Entrepreneurs
           </Title>
+
           <p className="text-muted-foreground">
             Find promising startups and entrepreneurs to invest in:
           </p>
--- src/app/dashboard/investor/page.tsx
+++ src/app/dashboard/investor/page.tsx
@@ -1,44 +1,30 @@
 "use client";
 
-import { mockCollaborationRequests, mockInvestors } from "@/lib/mock-data";
+import { mockInvestors } from "@/lib/mock-data";
 
 import List from "@components/common/List";
 import Title from "@components/common/Title";
 import InvestorCard from "@components/dashboard/InvestorCard";
 import DashboardLayout from "@/components/dashboard/DashboardLayout";
-import CollaborationRequestCard from "@components/dashboard/CollaborationRequestCard";
 
 const Page = () => {
   return (
     <DashboardLayout>
       <div className="mx-auto max-w-7xl p-6">
         <div className="mb-8">
-          <Title level="h1" ariaLevel={1} className="text-card-foreground mb-2 text-3xl font-bold">
+          <Title
+            level="h1"
+            ariaLevel={1}
+            className="text-card-foreground mb-2 text-4xl font-extrabold tracking-tight"
+          >
             Investor Dashboard
           </Title>
 
           <p className="text-muted-foreground">
-            Manage collaboration requests and discover investors.
+            Unlock tailored investment opportunities designed to accelerate your company `&apos;`s
+            growth and diversification.
           </p>
         </div>
-
-        <div className="mb-6">
-          <Title level="h2" className="text-card-foreground mb-2 text-2xl font-bold">
-            Collaboration Requests
-          </Title>
-        </div>
-
-        <List
-          as={"div"}
-          className="relative-grid mb-9"
-          items={mockCollaborationRequests}
-          renderItem={(collaborationRequest) => (
-            <CollaborationRequestCard
-              key={collaborationRequest.id}
-              userRequests={collaborationRequest}
-            />
-          )}
-        />
 
         <div>
           <Title
@@ -50,7 +36,7 @@ const Page = () => {
           </Title>
 
           <List
-            as={"div"}
+            as={"section"}
             className="relative-grid"
             items={mockInvestors}
             renderItem={(investor) => <InvestorCard key={investor.id} investor={investor} />}
